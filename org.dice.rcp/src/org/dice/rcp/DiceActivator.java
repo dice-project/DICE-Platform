64eb95a95ddcd829c1e4dd8cc5cc43ed6b0a99c3
Marc Gil <marcgise@gmail.com>
2016-02-25 16:37:02 +0100

    first commit

---
+++ org.dice.rcp/src/org/dice/rcp/DiceActivator.java
@@ -0,0 +1,34 @@
+package org.dice.rcp;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class DiceActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.
+	 * BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		DiceActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		DiceActivator.context = null;
+	}
+
+}
